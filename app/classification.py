eb24ba039f0ee0b18eba7f64e766e42113eeb228
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-08-31 23:53:46 +0530

    update: added classification response and model performance table

--- app/classification.py
+++ app/classification.py
@@ -12,3 +12,14 @@ def automl(df, label, usecase):
     wip = st.empty()  # Placeholder for the "Please wait" message
             
     wip.text('Building your models, Please Wait....')  # Show "Please wait" message
+
+    if usecase == "classification":
+        table, best = build_classifier(df,label)
+    else:
+        m1 = setup(data = df,target = label)
+        best = compare_models()
+        table = pull()
+    wip.empty()  # Remove "Please wait" message
+    st.subheader('Table of Model Performance')
+
+    st.write(pull())
