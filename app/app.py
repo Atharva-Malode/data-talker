638223e8105391007ac2788fa28971f7463b2790
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-08-31 20:10:14 +0530

    update: added radio button for navgating to visualize data

--- app/app.py
+++ app/app.py
@@ -8,6 +8,8 @@ from langchain.vectorstores import FAISS
 import tiktoken
 import tempfile
 import openai
+from instruction import instructions
+from eda import visualize
 
 st.set_page_config(
    page_title="Data-Talker",
@@ -29,69 +31,66 @@ def main():
             type="password")
 
         uploaded_file = st.sidebar.file_uploader("Upload CSV", type="csv")
-        
-        st.sidebar.title("Instructions")
-        st.sidebar.markdown("1. Enter your OpenAI API key")
-        st.sidebar.markdown("2. Upload a CSV file")
-        st.sidebar.markdown("3. Input queries and see responses")
-        st.sidebar.info("Note : We are working on introducing the LLAMA model by Meta AI for better results which would not need any API key. Please feel free to contact us for any queries, we would also appreciate open source contributions and sponsors.")
-        github_link = "[GitHub](https://github.com/Atharva-Malode/Data-Talker)"
-        st.sidebar.info("To contribute and Sponser - " + github_link)
 
         if uploaded_file:
             # Use tempfile because CSVLoader only accepts a file_path
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 tmp_file.write(uploaded_file.getvalue())
                 tmp_file_path = tmp_file.name
-
             loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
             data = loader.load()
 
             st.write("CSV Data:")
             st.write("Wait for some time it may take some time to load the data.....")
 
-            if user_api_key:
-                openai.api_key = user_api_key  # Set the API key
-                embeddings = OpenAIEmbeddings(openai_api_key=user_api_key)
-                vectorstore = FAISS.from_documents(data, embeddings)
+            options = st.sidebar.radio("What do you want to do?", ("Talk to your Data", "Visualize Data"))
 
-                chain = ConversationalRetrievalChain.from_llm(
-                    llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
-                    retriever=vectorstore.as_retriever())
-                            # Container for the chat history
-            response_container = st.container()
-            # Container for the user's text input
-            container = st.container()
+            if options == "Visualize Data":
+                visualize(uploaded_file)
+            if options == "Talk to your Data":
+                if user_api_key:
+                    openai.api_key = user_api_key  # Set the API key
+                    embeddings = OpenAIEmbeddings(openai_api_key=user_api_key)
+                    vectorstore = FAISS.from_documents(data, embeddings)
 
-            if 'history' not in st.session_state:
-                st.session_state['history'] = []
+                    chain = ConversationalRetrievalChain.from_llm(
+                        llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
+                        retriever=vectorstore.as_retriever())
+                                # Container for the chat history
+                response_container = st.container()
+                # Container for the user's text input
+                container = st.container()
 
-            if 'generated' not in st.session_state:
-                st.session_state['generated'] = ["Hello! Ask me anything about " + uploaded_file.name + " 🤗"]
+                if 'history' not in st.session_state:
+                    st.session_state['history'] = []
 
-            if 'past' not in st.session_state:
-                st.session_state['past'] = ["Hey! 👋"]
+                if 'generated' not in st.session_state:
+                    st.session_state['generated'] = ["Hello! Ask me anything about " + uploaded_file.name + " 🤗"]
 
-            with container:
-                with st.form(key='my_form', clear_on_submit=True):
-                    user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
-                    submit_button = st.form_submit_button(label='Send')
+                if 'past' not in st.session_state:
+                    st.session_state['past'] = ["Hey! 👋"]
 
-                if submit_button and user_input:
-                    output = conversational_chat(user_input, chain)
-                    st.session_state['past'].append(user_input)
-                    st.session_state['generated'].append(output)
+                with container:
+                    with st.form(key='my_form', clear_on_submit=True):
+                        user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+                        submit_button = st.form_submit_button(label='Send')
 
-            if st.session_state['generated']:
-                with response_container:
-                    for i in range(len(st.session_state['generated'])):
-                        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                        message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
-            else:
-                st.write("No data uploaded yet")
+                    if submit_button and user_input:
+                        output = conversational_chat(user_input, chain)
+                        st.session_state['past'].append(user_input)
+                        st.session_state['generated'].append(output)
 
-                # ... (rest of the code remains the same)
-    
+                if st.session_state['generated']:
+                    with response_container:
+                        for i in range(len(st.session_state['generated'])):
+                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+                            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                else:
+                    st.write("No data uploaded yet")
+
+        # Show Instructions
+        instructions()
+
     except openai.error.AuthenticationError:
         st.error("Invalid API key. Please check your OpenAI API key and try again.")
     except openai.error.RateLimitError:
