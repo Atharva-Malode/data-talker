fb309c99c5cbf19ed2ab285f2fb6de123e0ef420
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-08-31 23:50:08 +0530

    update: initialized automl function

--- app/classification.py
+++ app/classification.py
@@ -12,29 +12,3 @@ def automl(df, label, usecase):
     wip = st.empty()  # Placeholder for the "Please wait" message
             
     wip.text('Building your models, Please Wait....')  # Show "Please wait" message
-
-    if usecase == "classification":
-        table, best = build_classifier(df,label)
-    else:
-        m1 = setup(data = df,target = label)
-        best = compare_models()
-        table = pull()
-    wip.empty()  # Remove "Please wait" message
-    st.subheader('Table of Model Performance')
-
-    st.write(pull())
-
-    st.subheader('Plot of Model Performance')
-    fig = plt.figure(figsize=(15,6))
-    plt.xlabel('Models')
-    if usecase == "regression":
-        plt.bar(table['Model'].head(), table['R2'].head())
-        plt.ylabel('R Square')
-    else:
-        plt.bar(table['Model'].head(), table['Accuracy'].head())
-        plt.ylabel('Accuracy')
-    st.pyplot(fig)
-
-    st.subheader('Hyper Parameter Tuning')
-    tuned_model = tune_model(best)
-    st.write(pull())
